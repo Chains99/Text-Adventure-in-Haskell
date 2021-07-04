6e772635b54472b9b7d23a95806a4b321fec8a74
Chains <aylinalvarezsantos@gmail.com>
2021-07-04 15:30:48 -0400

    accent mark, docs

--- BattleOfAzeroth.hs
+++ BattleOfAzeroth.hs
@@ -7,7 +7,7 @@ gameIntro :: IO ()
 gameIntro = do
     putStrLn ">> El origen de la Horda se remonta a los antiguos clanes orcos de Draenor donde habían convivido pacíficamente con los draenei. Ner'zhul, uno de los chamanes orcos, contactó con un ente extradimensional llamado Kil'Jaeden que lo persuadió para que creyera que era un espíritu. A pesar de haber dedicado toda su vida al equilibrio y la naturaleza, Ner'zhul respondió a los cantos de sirena que el demonio le ofreció, abandonando sus enseñanzas y entregándose a una nueva senda de conocimiento: el de la brujería. Las consecuencias fueron salvajes. Una vez que hubo aprendido las técnicas más básicas de manipulación que su nueva magia le ofrecía, su fama creció entre sus coetáneos que abandonaron los viejos caminos del chamanismo y comenzaron a destruir la naturaleza a la que anteriormente habían servido."
     putStrLn ""
-    putStrLn ">> Tu objetivo sera realizar las misiones para ayudar a la Horda y ganarte un renombre en los distintos mundos\n"
+    putStrLn ">> Tu objetivo será realizar las misiones para ayudar a la Horda y ganarte un renombre en los distintos mundos\n"
 
 manageGame :: World -> IO()
 manageGame (World name episode story action nW kW )  =  do
